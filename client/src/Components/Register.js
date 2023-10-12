24e9d6d735d9832b88f1ac6891b53bc7e9c2880f
hillaryke <tenskap@gmail.com>
2023-10-12 14:20:13 +0300

    Register user successfully in client

--- client/src/Components/Register.js
+++ client/src/Components/Register.js
@@ -1,6 +1,13 @@
 import React, { useState } from 'react';
+import { Navigate, useNavigate } from "react-router-dom";
 
+import axios from "../utils/api";
 const Register = () => {
+
+  const navigate = useNavigate();
+
+  const [registeredUser, setRegisteredUser] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
   const [formData, setFormData] = useState({
     username: '',
     email: '',
@@ -13,10 +20,39 @@ const Register = () => {
     setFormData({ ...formData, [name]: value });
   };
 
-  const handleSubmit = (e) => {
+
+  const loginRequest = async ({ email, username, password }) => {
+    await axios.post('/users/register', { email, username, password })
+        .then((response) => {
+          // console.log(response)
+
+          if (response.status === 201) {
+            setRegisteredUser(response.data);
+            localStorage.setItem('token', response.data.access_token);
+            localStorage.setItem('user', JSON.stringify(response.data));
+
+            navigate('/')
+          }
+        }).catch((error) => {
+          // Handle error.
+          console.log(error);
+          if (error.response.status === 400) {
+            setErrorMessage(error.response.data.message);
+          }
+        });
+  }
+
+  const handleSubmit = async (e) => {
     e.preventDefault();
     // Handle form submission for user registration
     // Implement your actual registration logic
+
+
+    const response = await loginRequest({
+      email: formData.email,
+      username: formData.username,
+      password: formData.password,
+    });
   };
 
   return (
@@ -24,6 +60,7 @@ const Register = () => {
       <h1>Register</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
+          <h5>{errorMessage}</h5>
           <label htmlFor="username" className="form-label">Username</label>
           <input
             type="text"
